d6498b0499dc141607d2cf75b96100671c55b17d
Alain Perkaz <alainperkaz@gmail.com>
2018-02-05 17:03:44 +0100

    Add feather service support

--- app.js
+++ app.js
@@ -1,26 +1,86 @@
 const feathers = require('@feathersjs/feathers');
+
+class Messages {
+
+    constructor(){
+        this.messages = [];
+        this.currentId = 0;
+    }
+
+    async find(params){
+        // return list of parameters
+        return this.messages;
+    }
+
+    async get(id, params){
+        // find message by id
+        const message = this.messages.find(
+            message => message.id === parseInt(id, 10)
+        );
+
+        if(!message){
+            throw new Error(`Message with id ${id} not found`)
+        }
+
+        return message;
+    }
+
+    async create(data, params){
+        const message = Object.assign({
+            id: ++this.currentId
+        }, data);
+
+        this.messages.push(message);
+
+        return message;
+    }
+
+    async patch(id, data, params){
+        const message = await this.get(id);
+
+        return Object.assign(message, data);
+    }
+
+    async remove(id, params){
+        const message = await this.get(id);
+
+        const index = this.messages.indexOf(message);
+
+        this.messages.slice(index, 1);
+
+        return message;
+    }
+};
+
 const app = feathers();
 
-// Register a simple todo service that return the name and a text
-app.use('todos', {
-    async get(name) {
-        // Return an object in the form of { name, text }
-        return {
-            name,
-            text: `You have to do ${name}`
-        };
-    }
-});
+app.use('messages', new Messages());
 
-// A function that gets and logs a todo from the service
-async function getTodo(name) {
-    // Get the service we registered above
-    const service = app.service('todos');
-    // Call the `get` method with a name
-    const todo = await service.get(name);
 
-    // Log the todo we got back
-    console.log(todo);
+
+async function processMessages(){
+
+    app.service('messages').on('created', message => {
+        console.log('Created a new message: ', message);
+    });
+
+    app.service('messages').on('removed', message => {
+        console.log('Deleted message: ', message);
+    });
+
+    await app.service('messages').create({
+       text: 'First message'
+    });
+
+    const lastMessage = await app.service('messages').create({
+        text: 'Second message'
+    });
+
+    await app.service('messages').remove(lastMessage.id);
+
+    const messageList = await app.service('messages').find();
+
+    console.log('Available messages: ', messageList);
 }
 
-getTodo('dishes');
+processMessages();
