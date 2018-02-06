538cca87b62d4f5fb2463df1f4b89ce925689f42
Alain Perkaz <alainperkaz@gmail.com>
2018-02-06 11:12:03 +0100

    Add chat message NeDB service

--- chat-application/src/hooks/logger.js
+++ chat-application/src/hooks/logger.js
@@ -9,13 +9,13 @@ const logger = require('winston');
 module.exports = function () {
   return context => {
     // This debugs the service call and a stringified version of the hook context
-    // You can customize the mssage (and logger) to your needs
+    // You can customize the message (and logger) to your needs
     logger.debug(`${context.type} app.service('${context.path}').${context.method}()`);
-    
+
     if(typeof context.toJSON === 'function') {
       logger.debug('Hook Context', JSON.stringify(context, null, '  '));
     }
-    
+
     if (context.error) {
       logger.error(context.error);
     }
